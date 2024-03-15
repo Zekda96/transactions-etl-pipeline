8bb262b3aec59917ec994632190d7b9411174b9b
Zekda96 <danielgranjac@gmail.com>
2024-03-15 15:35:18 -0500

    edited comments

--- dags/load_data.py
+++ dags/load_data.py
@@ -5,35 +5,43 @@ import boto3
 from botocore import exceptions
 import logging
 
-# SQLite Database
+# Data Management
 import sqlite3
 import pandas as pd
+
+# Buffer
 import io
 
 
 def save_file_to_s3(client, fn):
     """
     Save file to LocalStack S3 bucket.
+    :param client: S3 client initialized using boto3.
+    :param fn: Name of the table to pull from and the final .parquet file
     """
+
     bucket_name = "zilliqa-transactions"
 
     logging.info('Uploading file')
-
+    # Get table from SQLite database and store it as a .parquet file on S3.
     try:
+        # Connect to SQlite database
         db_connection = sqlite3.connect('zilliqa.db')
+
+        # Pull data from table and load as DataFrame
         query = f'SELECT * FROM {fn}'
         df = pd.read_sql_query(query, db_connection)
 
+        # Create Buffer object from DataFrame
         bytes_io = io.BytesIO()
         df.to_parquet(bytes_io, index=False)
         bytes_io.seek(0)
 
+        # Store data on S3 bucket
         path = f"{fn}.parquet"
         client.put_object(Bucket=bucket_name, Key=path, Body=bytes_io)
-
-        # client.upload_file(fn, bucket_name, fn)
         logging.info(f'Successfully connected with Localstack S3')
-        logging.info(f'File {fn}uploaded to bucket: {bucket_name}')
+        logging.info(f'File {fn} uploaded to bucket: {bucket_name}')
 
     except exceptions.ClientError as e:
         logging.error('Upload failed.')
